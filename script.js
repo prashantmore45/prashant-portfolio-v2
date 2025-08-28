203a2d133dd2706fea63ae8cf9ed56d747b30405
prashantmore45 <prashantmorepm05@gmail.com>
2025-08-28 12:14:57 +0530

    upadted script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
+const navLinks = document.querySelector(".right");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
