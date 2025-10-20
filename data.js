f29ee0d92945e27aa9631dab7b9e8757863c1af5
prashantmore45 <prashantmorepm05@gmail.com>
2025-10-20 21:03:01 +0530

    feat(portfolio): add dynamic project loading and restructure project section

---
+++ data.js
@@ -0,0 +1,30 @@
+export const projects = [
+    {
+        id: '1',
+        image: 'images/amazon clone.png',
+        name: 'Amazon Clone',
+        description: "A fully responsive clone of Amazon's homepage, checkout page, order page and tracking page created using HTML, CSS and JavaScript.",
+        link: "https://github.com/prashantmore45/Amazon-Clone"
+    }, 
+    {
+        id: '2',
+        image: 'images/tictac.png',
+        name: 'Tic Tac Toe Game',
+        description: "A classic two-player game built with HTML, CSS, and JavaScript. Includes interactive game board, win detection logic, and a reset button for replayability.",
+        link: "https://github.com/prashantmore45/Tic-Tac-Toe-Game"
+    },
+    {
+        id: '3',
+        image: 'images/rockpaper.png',
+        name: 'Rock Paper Scissors Game',
+        description: "An interactive game where you play against the computer. Built using HTML, CSS, and JavaScript with real-time score tracking and dynamic result display.",
+        link: "https://github.com/prashantmore45/Rock-Paper-Scissors-Game"
+    },
+    {
+        id: '4',
+        image: 'images/simple cal.png',
+        name: 'Simple Calculato',
+        description: "A functional calculator built with HTML, CSS, and JavaScript. Supports basic arithmetic operations and a clean, responsive UI for smooth usage.",
+        link: "https://github.com/prashantmore45/Simple-Calculator"
+    }
+];
--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+import { projects } from "./data.js";
+
+
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".right");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+
+let projectsHTML = '';
+
+projects.forEach((project) => {
+  projectsHTML += `
+    <div class="project-card" data-id="${project.id}">
+      <img src="${project.image}" alt="Amazon Clone Project Screenshot">
+      <h3>${project.name}</h3>
+      <p>${project.description}</p>
+      <a href="${project.link}" target="_blank">View on GitHub</a>
+    </div>`;
+  }
+)
+
+document.querySelector(".project-container").innerHTML = projectsHTML;
