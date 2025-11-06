b3fcfecf15a2e94f5dd86165fa7a63d37dc33ba8
prashantmore45 <prashantmorepm05@gmail.com>
2025-11-06 17:55:03 +0530

    feat(portfolio): add demo links for several projects and ensure consistent data structure

--- data.js
+++ data.js
@@ -4,7 +4,8 @@ export const projects = [
         image: 'images/amazon clone.png',
         name: 'Amazon Clone',
         description: "A fully responsive clone of Amazon's homepage, checkout page, order page and tracking page created using HTML, CSS and JavaScript.",
-        link: "https://github.com/prashantmore45/Amazon-Clone"
+        link: "https://github.com/prashantmore45/Amazon-Clone",
+        demo: "Coming Soon"
     }, 
     {
         id: '2',
@@ -19,25 +20,29 @@ export const projects = [
         image: 'images/rockpaper.png',
         name: 'Rock Paper Scissors Game',
         description: "An interactive game where you play against the computer. Built using HTML, CSS, and JavaScript with real-time score tracking and dynamic result display.",
-        link: "https://github.com/prashantmore45/Rock-Paper-Scissors-Game"
+        link: "https://github.com/prashantmore45/Rock-Paper-Scissors-Game",
+        demo: "https://prashantmore45.github.io/Rock-Paper-Scissors-Game/"
     },
     {
         id: '4',
         image: 'images/simple cal.png',
         name: 'Simple Calculator',
         description: "A functional calculator built with HTML, CSS, and JavaScript. Supports basic arithmetic operations and a clean, responsive UI for smooth usage.",
-        link: "https://github.com/prashantmore45/Simple-Calculator"
+        link: "https://github.com/prashantmore45/Simple-Calculator",
+        demo: "Coming Soon"
     }, {
         id: '5',
         image: 'images/aiimage.png',
         name: 'AI Image Generator',
         description: "A web application that generates images based on user prompts using OpenAI's DALL·E API. Built with HTML, CSS, and JavaScript.",
-        link: "https://github.com/prashantmore45/AI-Image-Generator"
+        link: "https://github.com/prashantmore45/AI-Image-Generator",
+        demo: "Coming Soon"
     }, {
         id: '6',
         image: 'images/aichatbot.png',
         name: 'AI Chatbot',
         description: "A web-based AI chatbot application that utilizes Google Gemini's API to provide intelligent and context-aware responses to user queries. Built with HTML, CSS, and JavaScript.",
-        link: "https://github.com/prashantmore45/AI---Chatbot"
+        link: "https://github.com/prashantmore45/AI---Chatbot",
+        demo: "Coming Soon"
     }
 ];
